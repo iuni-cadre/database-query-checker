cc789972f08ec9028ab7985a73e27b28d3f493c5
Aditya Gupta <contactadityag@gmail.com>
2019-09-27 15:42:21 -0400

    Commits the script to connect to the database and sets up the repository

---
+++ metadatabase_config.py
@@ -0,0 +1,9 @@
+# config file containing details of the Cadre Metadatabase instance
+
+database_host = ***REMOVED***
+database_port = ***REMOVED***
+database_username = ***REMOVED***
+database_password = ***REMOVED***
+database_name = ***REMOVED***
+
+
