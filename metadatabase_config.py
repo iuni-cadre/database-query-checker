5845a7a60c026d33af12105dd0858c277e4e210a
ADS\pkamburu <pkamburu@iu.edu>
2023-02-24 15:14:41 -0500

    remove database configuration

--- metadatabase_config.py
+++ metadatabase_config.py
@@ -1,9 +1,9 @@
 # config file containing details of the Cadre Metadatabase instance
 
-database_host = ***REMOVED***
-database_port = ***REMOVED***
-database_username = ***REMOVED***
-database_password = ***REMOVED***
-database_name = ***REMOVED***
+database_host = "DATABASE_HOST"
+database_port = DATABASE_PORT
+database_username = "DATABASE_USERNAME"
+database_password = "DATABASE_PASSWORD"
+database_name = "DATABASE_NAME"
 
 
